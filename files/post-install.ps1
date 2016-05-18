499973730f71ab7bf3159f4f02651e70793f96d4
joelschoenberg <joel.schoenberg@hotmail.com>
2016-05-17 21:28:24 -0700

    Update to apply the ToLower() function to the certname string value
    This avoids the problem of using a certname that begins with a capital letter.

--- files/post-install.ps1
+++ files/post-install.ps1
@@ -1,9 +1,8 @@
 $puppet_master=$args[0]
 $puppet_role=$args[1]
 # This script lays down a yaml fact to designate a machine's role and then 
-# installs the windows puppet agent on the windows seteam vagrant vms
-# from the master's pe_repo by downloading it to C:\tmp first and then running
-# msiexec on it from there.
+# installs the windows puppet agent from the master's pe_repo by downloading 
+# it to C:\tmp first and then running msiexec on it from there.
 
 New-Item C:\ProgramData\PuppetLabs\facter\facts.d -type directory -force
 New-Item C:\ProgramData\PuppetLabs\facter\facts.d\role.yaml -type file -value "puppet_role: ${puppet_role}" -force
